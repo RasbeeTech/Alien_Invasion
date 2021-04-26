f2d16ec30d75b1017d3f49a1b4e7261a01908ef9
MykalB081 <mykalb081@gmail.com>
2021-04-26 16:23:28 -0400

    Update alien_invasion.py
    - Back to window mode

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,11 +14,11 @@ class AlienInvasion:
         self.settings = Settings()
 
         # Run Alien Invasion in full-screen mode
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         # Run Alien Invasion in a window
-        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
         self.ship = Ship(self)
